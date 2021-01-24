b91272fca73befd9324fa7558d0309615495a7b3
JonShort <JonShort@users.noreply.github.com>
2021-01-24 19:45:32 +0000

    Add score / game over

--- preload.js
+++ preload.js
@@ -1,36 +1,50 @@
-const { mathProblem } = require('./methods');
+const { mathProblem } = require("./methods");
 
 // All of the Node.js APIs are available in the preload process.
 // It has the same sandbox as a Chrome extension.
-window.addEventListener('DOMContentLoaded', () => {
+window.addEventListener("DOMContentLoaded", () => {
   const replaceText = (selector, text) => {
-    const element = document.getElementById(selector)
-    if (element) element.innerText = text
-  }
+    const element = document.getElementById(selector);
+    if (element) element.innerText = text;
+  };
 
   let currAnswer = 0;
+  let score = 0;
+  let rollingQ = 0;
+
+  const form = document.getElementById("form");
+  const input = document.getElementById("answer");
 
   const newMathProblem = () => {
+    rollingQ++;
     const { text, answer } = mathProblem();
-  
+
     replaceText("question", text);
     currAnswer = answer;
-  }
+    input.value = "";
+  };
 
-  const form = document.getElementById('form');
-  form.addEventListener('submit', (ev) => {
-    const answer = parseInt(ev.target.elements['answer'].value);
+  form.addEventListener("submit", (ev) => {
+    ev.preventDefault();
+
+    let statement = "Incorrect";
+    const answer = parseInt(ev.target.elements["answer"].value);
 
     if (answer === currAnswer) {
-      alert(`Correct! Answer was ${currAnswer}`);
-      return;
+      score++;
+      statement = "Correct";
     }
 
-    alert(`Incorrect! Answer was ${currAnswer}`);
-  })
+    alert(`${statement}! Answer was ${currAnswer} - score ${score}`);
+
+    if (rollingQ >= 5) {
+      alert(`Game over! You scored ${score}/${rollingQ}`);
+    }
+
+    newMathProblem();
+  });
 
   newMathProblem();
 
-  const input = document.getElementById('answer');
   input.focus();
-})
+});
